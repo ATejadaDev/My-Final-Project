374a2b437f40d932dd202f8c654bfef87c8f9c40
Angel Tejada <angelfernan018@gmail.com>
2022-07-23 18:35:15 +0200

    First commit (home + checkout finished)

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC7rHeEdFtR5wN-Fu-RbLTp1anyZSZfu3Q",
+    authDomain: "my-shop-27e53.firebaseapp.com",
+    projectId: "my-shop-27e53",
+    storageBucket: "my-shop-27e53.appspot.com",
+    messagingSenderId: "728270742275",
+    appId: "1:728270742275:web:fc64da8a49b09f45aa7f36",
+    measurementId: "G-LL2X5G9B47"
+  };

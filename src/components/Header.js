e610664677233c8e47cbcd0e0832befc170afc1a
Angel Tejada <angelfernan018@gmail.com>
2022-07-25 16:44:41 +0200

    Users finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,34 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from 'react-router-dom';
 import { useStateValue } from './StateProvider';
+import { auth } from '../firebase';
 
 function Header() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{basket, user}, dispatch] = useStateValue();
 
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
   return (
     <div className='header'>
     <Link to={"/"}>
-      <img className="header__logo" src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' alt='XD'/>
+      <img className="header__logo" src='https://www.nicepng.com/png/full/16-167642_amazon-logo-amazon-logo-white-text.png' alt='XD'/>
     </Link>
       <div className='header_search'>
         <input className='header__searchInput' type="text"></input>
         <SearchIcon className='header__searchIcon' />
       </div>
-      <div className='header__nav'>
-        <div className='header__option'>
-          <span className='header__optionLineOne'>Hello Guest</span>
-          <span className='header__optionLineTwo'>Sign In</span>
-        </div>
+
+        <div className='header__nav'>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className='header__option'>
+            <span className='header__optionLineOne'>Hello Guest</span>
+            <span className='header__optionLineTwo'>{user ? "Sign Out" : "Sign In"}</span>
+          </div>
+        </Link>
+
         <div className='header__option'>
           <span className='header__optionLineOne'>Returns</span>
           <span className='header__optionLineTwo'>& Orders</span>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyC7rHeEdFtR5wN-Fu-RbLTp1anyZSZfu3Q",
@@ -8,3 +12,10 @@ const firebaseConfig = {
     appId: "1:728270742275:web:fc64da8a49b09f45aa7f36",
     measurementId: "G-LL2X5G9B47"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };

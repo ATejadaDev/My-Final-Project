34db7a26cfa6656ae65909736580ad15ef6a4139
Angel Tejada <angelfernan018@gmail.com>
2022-07-26 20:41:59 +0200

    Fixed routes and added users

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
 import React, { useEffect } from "react";
-import './App.css';
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Checkout from "./components/Checkout";
-import Login from "./components/Login";
-import { auth } from "./firebase";
-import { useStateValue } from "./components/StateProvider";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
   const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
-    // Solo funcionara cuando te logues
+    // will only run once when the app component loads...
 
     auth.onAuthStateChanged(authUser => {
-      console.log("El usuario es ->", authUser);
+      console.log('THE USER IS >>> ', authUser);
 
       if (authUser) {
-      //El usuario esta logeado o estaba 
+        // the user just logged in / the user was logged in
+
         dispatch({
-          type: "SET_USER",
+          type: 'SET_USER',
           user: authUser
         })
       } else {
-        // The user is logged out
+        // the user is logged out
         dispatch({
-          type: "SET_USER",
+          type: 'SET_USER',
           user: null
         })
       }
     })
   }, [])
+
   return (
     // BEM
-      <Router>
-        <div className="app">
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/checkout">
-              <Header />
-              <Checkout />
-            </Route>
-            <Route path="/">
-              <Header />
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+             <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+             <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/61Z1G7PtqYL._SX3000_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          <Product
+            id="12321341"
+            title="Rune Factory 5 Limited Edition"
+            price={59.99}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/813Pi5Ous9L._AC_SX569_.jpg"
+          />
+          <Product
+            id="49538094"
+            title="Echo Dot (3.ª generación) - Altavoz inteligente con Alexa, tela de color antracita"
+            price={33.99}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/61u48FEs0rL._AC_SY200_.jpg"
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="4903850"
+            title="PlayStation 5 - Mando inalámbrico DualSense - Exclusivo para PS5"
+            price={199.99}
+            rating={3}
+            image="https://m.media-amazon.com/images/I/51KNE0DS4vS._AC_SX679_.jpg"
+          />
+          <Product
+            id="23445930"
+            title="SanDisk SDSSDA-240G Plus – Disco sólido interno de 240 GB, SATA III SSD, con hasta 530 MB/s, Color Negro"
+            price={98.99}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/71J4Q8zM72L._AC_SX450_.jpg"
+          />
+          <Product
+            id="3254354345"
+            title="Echo Dot (4.ª generación) | Altavoz inteligente con Alexa | Antracita"
+            price={33.66}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/71Q9d6N7xkL._AC_SX679_.jpg"
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Nintendo Switch - Consola Estándar, Color Azul Neón/Rojo Neón"
+            price={285.00}
+            rating={1}
+            image="https://m.media-amazon.com/images/I/71U1R-V8+dL._AC_SX679_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
